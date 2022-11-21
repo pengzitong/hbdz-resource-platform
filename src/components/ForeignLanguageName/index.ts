a58008bd2dd8a030b2727f628f5f2b0a139b6591
彭梓桐ZIY00157915 <pengxt@jzteyao.com>
2022-11-21 16:12:02 +0800

    修复打包后组件不显示问题

--- src/components/ForeignLanguageName/index.ts
+++
@@ -1,9 +0,0 @@
-import ForeignLanguageName from './index.vue'
-
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-ForeignLanguageName.install = Vue => {
-  Vue.component(ForeignLanguageName.name, ForeignLanguageName)
-}
-
-export default ForeignLanguageName
